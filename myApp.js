7c274bcbd370ff2030bc75deea55fd1924887d68
solarjoni <>
2021-05-31 19:41:31 +0000

    Chain Search Query Helpers to Narrow Search ResultsPassed

--- myApp.js
+++ myApp.js
@@ -99,7 +99,14 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch})
+    .sort({name: 1})
+    .limit(2)
+    .select({age: 0})
+    .exec(function(err, people) {
+      if(err) return console.log(err);
+      done(null, people);
+    });
 };
 
 /** **Well Done !!**
